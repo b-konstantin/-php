5abb2626c07d198d843b832130fb7fc061cf04a6
b-konstantin <60023099+b-konstantin@users.noreply.github.com>
2020-01-25 09:08:26 +0700

    Решение задач
    Решены задачи на синтаксис от 1 по 7, так же на циклы от 1 до 4

---
+++ index.php
@@ -0,0 +1,61 @@
+<?php
+/*//Задача 1
+$htmllab='ru';
+echo "$htmllab";*/
+/*//Задача 2
+define("NPP", "10");
+echo NPP;*/
+/*//ЗАДАЧА 3
+$a=42;
+$b=28;
+echo $a+$b;
+
+$a=67;
+$b=45;
+echo $a-$b;
+
+$a=2;
+$b=9;
+echo $a*$b;
+
+$a=152;
+$b=8;
+echo $a/$b;*/
+
+/*//Задача 4
+$test=345;
+echo $test."<br/>";
+echo "<br/>";
+echo gettype($test)."<br/>";*/
+
+/*//Задача 5
+$test=(int)"345";
+echo gettype($test);*/
+/*//Задача 6
+echo "103"+2 //выведет 105*/
+/*//Задача 7
+$day="Среда";
+switch ($day){
+    case "Вторник";
+        echo "Сегодня Вторник";
+        break;
+    case "Среда";
+        echo "Сегодня Среда";
+        break;
+}*/
+/*//Циклы –задачи
+//Задача 1
+for ($i = 1; $i <= 100; $i ++) {
+    echo $i." ";}*/
+/*//задача 2
+for ($i = 23; $i <= 78; $i ++) {
+    echo $i." ";}*/
+/*//Задача 4
+<?php
+for($i=0;$i<100;$i++){$mas[$i]=rand(0,100);}
+for($i=0;$i<100;$i++){print $mas[$i];print"<br>";}*/
+?>
+
+
+
+
